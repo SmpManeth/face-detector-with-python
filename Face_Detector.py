a98e85a8b4be4d7a3ce29527aac6036dc0470cbf
Maneth Pathirana <manith.sampath@towcha.com.au>
2022-07-28 13:40:26 +0530

    detect one image with faces

--- Face_Detector.py
+++ Face_Detector.py
@@ -9,9 +9,16 @@ img = cv2.imread('rdj.jpg')
 #convert image to grey scale
 grayscale_image = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 
+#detect faces
+face_coordinates =tained_face_data.detectMultiScale(grayscale_image)
+
+
+print(face_coordinates)
+
 
 #show the Image
-cv2.imshow('Color Changer', grayscale_image)
+#cv2.imshow('Color Changer', grayscale_image)
 cv2.waitKey()
+
 
 print("Code Completed") 
