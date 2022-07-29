4048b5600e15c8b99b6a22a3dbbf0077357d1e20
Maneth Pathirana <manith.sampath@towcha.com.au>
2022-07-29 08:13:53 +0530

    face detection bugs fixed

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,24 +1,31 @@
+from ast import While
 import cv2
+from random import randrange  # getting random colors
 
-#load some pre trained data
+# load some pre trained data
 tained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#choose an Image to detect a face
-img = cv2.imread('rdj.jpg')
+# choose an Image to detect a face
+# img = cv2.imread('rdj.jpg')
 
-#convert image to grey scale
-grayscale_image = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
-
-#detect faces
-face_coordinates =tained_face_data.detectMultiScale(grayscale_image)
+# capture Video from webcam
+webcam = cv2.VideoCapture(0)
 
 
-print(face_coordinates)
-
-
-#show the Image
-#cv2.imshow('Color Changer', grayscale_image)
-cv2.waitKey()
-
-
-print("Code Completed") 
+while True:
+    succesful_frame_read, frame = webcam.read()
+    # convert image to grey scale
+    grayscale_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # detect faces and get the face cordonatres
+    face_coordinates = tained_face_data.detectMultiScale(grayscale_image)
+    
+    for (x, y, w, h) in face_coordinates:
+        # draw the rectangel
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,256, 0), 2)
+        
+    cv2.imshow('Detect Faces', frame)
+    cv2.waitKey(1)
+    if cv2.waitKey(1) == 81 or cv2.waitKey(1) == 113:
+      break
+webcam.release()
